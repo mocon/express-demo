b36f4988c24cff389cbf32b14447b2fa5705a2f2
Myles O'Connor <myles@drocventures.com>
2015-11-13 08:24:39 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    app = express();
+    
+app.get('/', function(req, res){
+  res.send({
+    page: "home",
+    response: "home json"
+  });
+});
+
+app.get('/test', function(req, res){
+  res.send({
+    page: "test",
+    response: "test json"
+  });
+});
+
+var server = app.listen(3000, function(){
+  var host = server.address().address,
+      port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
